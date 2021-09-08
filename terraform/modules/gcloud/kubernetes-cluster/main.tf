adc3bfe0318e8f3a8595dfdb525b93a611f57d83
willpxxr <williamparr96@outlook.com>
2021-09-08 20:12:12 +0100

    Remove dedicated node pool, bump machine type

--- terraform/modules/gcloud/kubernetes-cluster/main.tf
+++ terraform/modules/gcloud/kubernetes-cluster/main.tf
@@ -13,34 +13,10 @@ resource "google_container_node_pool" "this" {
 
   node_config {
     preemptible = true
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-2"
     disk_size_gb = var.node_pool_disk_size
     // TODO: Hackuiri Daiquiri - No obvious way to perform mappings using TF yet...
     disk_type = var.node_pool_disk_type == "HHD" ? "pd-standard" : var.node_pool_disk_type == "SSD" ? "pd-ssd" : null
-  }
-
-  depends_on = [
-    google_container_cluster.this
-  ]
-}
-
-resource "google_container_node_pool" "this_ingress_node_pool" {
-  cluster = google_container_cluster.this.name
-
-  name = format("%s-ingress-node-pool", var.name)
-  node_count = 1
-
-  node_config {
-    preemptible = true
-    machine_type = "e2-micro"
-    disk_size_gb = 10
-    disk_type = "pd-standard"
-
-    taint {
-      effect = "NO_SCHEDULE"
-      key = "dedicated"
-      value = "ingress"
-    }
   }
 
   depends_on = [
